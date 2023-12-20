0cb66314c14ae23f7a925715067516c1d93f4697
jannfis <jann@mistrust.net>
2023-12-20 13:19:29 +0000

    Add tests for clock package
    Signed-off-by: jannfis <jann@mistrust.net>

--- internal/clock/clock_test.go
+++ internal/clock/clock_test.go
@@ -3,9 +3,12 @@ package clock
 import (
 	"testing"
 	"time"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
-func Test_StandarClock(t *testing.T) {
+func Test_StandadClock(t *testing.T) {
 	clock := StandardClock()
 	var now, cnow time.Time
 	// For the unlikely event that now and cnow are being called in another
@@ -17,4 +20,16 @@ func Test_StandarClock(t *testing.T) {
 			break
 		}
 	}
+}
+
+func Test_SeededClock(t *testing.T) {
+	t.Run("Seeded clock", func(t *testing.T) {
+		seed, err := time.Parse(time.RFC3339, "2023-12-12T00:01:00Z")
+		require.NoError(t, err)
+		clock := SeededClock(seed)
+		now := clock.Now()
+		assert.Equal(t, 2023, now.Year())
+		assert.Equal(t, time.Month(12), now.Month())
+		assert.Equal(t, 12, now.Day())
+	})
 }
