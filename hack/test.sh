bc2718d7ae3092b83e24775dfdb6f7d5f1e31a4f
jannfis <jann@mistrust.net>
2024-06-13 20:11:26 +0000

    build(test): Add Codecov reports
    Signed-off-by: jannfis <jann@mistrust.net>

--- hack/test.sh
+++ hack/test.sh
@@ -2,4 +2,4 @@
 # Wrapper script to run unit tests for argocd-agent
 set -eo pipefail
 UNIT_LIST=$(go list ./... | grep -E -v '(mock|mocks|e2e|fake|pkg/api/grpc)')
-go test -race -timeout 10m -coverprofile test/out/test.coverage $UNIT_LIST
+go test -race -timeout 10m -coverprofile test/out/coverage.out $UNIT_LIST
