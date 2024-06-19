92eb79762620e2c6a0fe7412ef11482c9010a558
Jann Fischer <jann@mistrust.net>
2024-06-19 19:30:04 -0400

    chore(scripts): Fix autonomous agent startup script (#87)
    Signed-off-by: jannfis <jann@mistrust.net>

--- hack/demo-env/start-agent-autonomous.sh
+++ hack/demo-env/start-agent-autonomous.sh
@@ -6,4 +6,4 @@ if ! kubectl config get-contexts | tail -n +2 | awk '{ print $2 }' | grep -qE '^
 fi
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 test -f cmd/agent/main.go || (echo "Script should be run from argocd-agent's root path" >&2; exit 1)
-go run ./cmd/agent/main.go --agent-mode autonomous --creds userpass:${SCRIPTPATH}/creds/creds.agent-autonomous --server-address 127.0.0.1 --server-port 8443 --insecure-tls --kubecontext vcluster-agent-autonomous --namespace agent-autonomous
+go run ./cmd/agent/main.go --agent-mode autonomous --creds userpass:${SCRIPTPATH}/creds/creds.agent-autonomous --server-address 127.0.0.1 --server-port 8443 --insecure-tls --kubecontext vcluster-agent-autonomous --namespace argocd
