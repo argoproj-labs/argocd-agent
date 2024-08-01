0320d894c486b83a7fe2d193e25ed41e9e486655
jannfis <jann@mistrust.net>
2024-08-01 14:22:03 +0000

    chore(demo-env): Allow command line args for startup scripts
    Signed-off-by: jannfis <jann@mistrust.net>

--- hack/demo-env/start-agent-autonomous.sh
+++ hack/demo-env/start-agent-autonomous.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 set -e -o pipefail
+ARGS=$*
 if ! kubectl config get-contexts | tail -n +2 | awk '{ print $2 }' | grep -qE '^vcluster-agent-autonomous$'; then
     echo "kube context vcluster-agent-autonomous is not configured; missing setup?" >&2
     exit 1
 fi
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 test -f cmd/agent/main.go || (echo "Script should be run from argocd-agent's root path" >&2; exit 1)
-go run ./cmd/agent/main.go --agent-mode autonomous --creds userpass:${SCRIPTPATH}/creds/creds.agent-autonomous --server-address 127.0.0.1 --server-port 8443 --insecure-tls --kubecontext vcluster-agent-autonomous --namespace argocd
+go run ./cmd/agent/main.go --agent-mode autonomous --creds userpass:${SCRIPTPATH}/creds/creds.agent-autonomous --server-address 127.0.0.1 --server-port 8443 --insecure-tls --kubecontext vcluster-agent-autonomous --namespace argocd $ARGS
