157efe2734095ee1b5b35ea65c8d039864e92620
Chetan Banavikalmutt <chetanrns1997@gmail.com>
2024-10-02 18:24:04 +0530

    Fix lint in test/proxy
    Signed-off-by: Chetan Banavikalmutt <chetanrns1997@gmail.com>

--- test/proxy/proxy.go
+++ test/proxy/proxy.go
@@ -34,7 +34,7 @@ func StartHTTP2DowngradingProxy(t *testing.T, addr string, target string) *http.
 		Addr:    lis.Addr().String(),
 	}
 
-    //nolint:errcheck
+	//nolint:errcheck
 	go server.ServeTLS(lis, basePath+".crt", basePath+".key")
 
 	return server
