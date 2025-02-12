3b6f1f3d07e21cdc4db88e999143e44e8d88438d
jannfis <jann@mistrust.net>
2025-02-12 23:40:16 +0000

    Add missing license headers
    Signed-off-by: jannfis <jann@mistrust.net>

--- cmd/ctl/ca.go
+++ cmd/ctl/ca.go
@@ -1,3 +1,17 @@
+// Copyright 2025 The argocd-agent Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package main
 
 import (
@@ -94,7 +108,7 @@ func NewCAGenerateCommand() *cobra.Command {
 					Name:      config.SecretNamePrincipalCA,
 					Namespace: globalOpts.namespace,
 				},
-				Type: "kubernetes.io/tls",
+				Type: corev1.SecretTypeTLS,
 				Data: map[string][]byte{
 					"tls.crt": []byte(cert),
 					"tls.key": []byte(key),
