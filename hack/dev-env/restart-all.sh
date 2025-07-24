1c733a071da68f6576b71de437b89869779ad3e5
Jann Fischer <jann@mistrust.net>
2025-07-24 15:11:05 -0400

    feat: Cluster installation utilities (#470)
    Signed-off-by: jannfis <jann@mistrust.net>

---
+++ hack/dev-env/restart-all.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+set -eo pipefail
+
+kubectl --context vcluster-control-plane -n argocd rollout restart deployment argocd-server
+kubectl --context vcluster-control-plane -n argocd rollout status --watch deployment argocd-server
+kubectl --context vcluster-control-plane -n argocd rollout restart deployment argocd-repo-server
+kubectl --context vcluster-control-plane -n argocd rollout status --watch deployment argocd-repo-server
+kubectl --context vcluster-control-plane -n argocd rollout restart deployment argocd-agent-principal
+kubectl --context vcluster-control-plane -n argocd rollout status --watch deployment argocd-agent-principal
+kubectl --context vcluster-agent-managed -n argocd rollout restart deployment argocd-agent-agent
+kubectl --context vcluster-agent-managed -n argocd rollout status --watch deployment argocd-agent-agent
+kubectl --context vcluster-agent-autonomous -n argocd rollout restart deployment argocd-agent-agent
+kubectl --context vcluster-agent-autonomous -n argocd rollout status --watch deployment argocd-agent-agent
+
