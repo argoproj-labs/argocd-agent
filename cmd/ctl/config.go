012cf5a69b6d1bc39231d4137eeff43ea4368403
Christopher Coco <ccoco@redhat.com>
2026-03-13 11:42:01 -0400

    fix: refactor to use cmdutil.ReadFromTerm
    Signed-off-by: Christopher Coco <ccoco@redhat.com>

--- cmd/ctl/config.go
+++ cmd/ctl/config.go
@@ -525,31 +525,13 @@ func CreateLocalConfig(kubeConfigPath, outputPath string) {
 	cfg.Contexts.Principals = make(map[string]componentConfig)
 	cfg.Contexts.Agents = make(map[string]componentConfig)
 	for context := range kubeConfig.Contexts {
-		var contextType string
-		var namespace string
-		var name string
 
-		fmt.Printf("Found context %s, is this a principal or agent, if it is neither type leave blank to skip? ", context)
-		_, err := fmt.Scanln(&contextType)
+		contextPrompt := fmt.Sprintf("Found context %s, is this a principal or agent? If is neither type leave blank to skip.", context)
+		contextType, err := cmdutil.ReadFromTerm(contextPrompt, -1, func(s string) bool {
+			return s == "principal" || s == "agent" || s == ""
+		})
 		if err != nil {
-			if err.Error() == "unexpected newline" {
-				contextType = ""
-			} else {
-				cmdutil.Fatal("Error reading input: %v", err)
-			}
-		}
-		contextType = strings.TrimSpace(contextType)
-
-		for contextType != "principal" && contextType != "agent" && contextType != "" {
-			fmt.Print("Invalid input, please enter principal, agent, or leave blank to skip: ")
-			_, err = fmt.Scanln(&contextType)
-			if err != nil {
-				if err.Error() == "unexpected newline" {
-					contextType = ""
-				} else {
-					cmdutil.Fatal("Error reading input: %v", err)
-				}
-			}
+			cmdutil.Fatal("%v", err)
 		}
 
 		if contextType == "" {
@@ -557,63 +539,38 @@ func CreateLocalConfig(kubeConfigPath, outputPath string) {
 			continue
 		}
 
-		fmt.Printf("Which namespace is the %s installed in? ", contextType)
-		_, err = fmt.Scanln(&namespace)
+		nsPrompt := fmt.Sprintf("Which namespace is the %s installed in?", contextType)
+		namespace, err := cmdutil.ReadFromTerm(nsPrompt, -1, func(s string) bool {
+			s = strings.TrimSpace(s)
+			return s != "" && !strings.Contains(s, " ")
+		})
 		if err != nil {
-			cmdutil.Fatal("Error reading input: %v", err)
+			cmdutil.Fatal("%v", err)
 		}
 		namespace = strings.TrimSpace(namespace)
 
-		for namespace == "" {
-			fmt.Print("Invalid input, namespace cannot be blank: ")
-			_, err = fmt.Scanln(&namespace)
-			if err != nil {
-				cmdutil.Fatal("Error reading input: %v", err)
+		namePrompt := fmt.Sprintf("What name would you like to use for this %s?", contextType)
+		name, err := cmdutil.ReadFromTerm(namePrompt, -1, func(s string) bool {
+			s = strings.TrimSpace(s)
+			var exists bool
+			switch contextType {
+			case "principal":
+				_, exists = cfg.Contexts.Principals[s]
+			case "agent":
+				_, exists = cfg.Contexts.Agents[s]
 			}
-			namespace = strings.TrimSpace(namespace)
-		}
-
-		fmt.Printf("What name would you like to use for this %s? ", contextType)
-		_, err = fmt.Scanln(&name)
+			return s != "" && !strings.Contains(s, " ") && !exists
+		})
 		if err != nil {
-			cmdutil.Fatal("Error reading input: %v", err)
+			cmdutil.Fatal("%v", err)
 		}
 		name = strings.TrimSpace(name)
-
-		for name == "" {
-			fmt.Print("Invalid input, name cannot be blank: ")
-			_, err = fmt.Scanln(&name)
-			if err != nil {
-				cmdutil.Fatal("Error reading input: %v", err)
-			}
-			name = strings.TrimSpace(name)
-		}
 
 		componentCfg := componentConfig{KubeContext: context, Namespace: namespace}
 		switch contextType {
 		case "principal":
-			_, exists := cfg.Contexts.Principals[name]
-			for exists {
-				fmt.Printf("Name for principal, %s, already exists. Enter a new name: ", name)
-				_, err = fmt.Scanln(&name)
-				if err != nil {
-					cmdutil.Fatal("Error reading input: %v", err)
-				}
-				name = strings.TrimSpace(name)
-				_, exists = cfg.Contexts.Principals[name]
-			}
 			cfg.Contexts.Principals[name] = componentCfg
 		case "agent":
-			_, exists := cfg.Contexts.Agents[name]
-			for exists {
-				fmt.Printf("Name for agent, %s, already exists. Enter a new name: ", name)
-				_, err = fmt.Scanln(&name)
-				if err != nil {
-					cmdutil.Fatal("Error reading input: %v", err)
-				}
-				name = strings.TrimSpace(name)
-				_, exists = cfg.Contexts.Agents[name]
-			}
 			cfg.Contexts.Agents[name] = componentCfg
 		}
 
@@ -625,22 +582,16 @@ func CreateLocalConfig(kubeConfigPath, outputPath string) {
 			fmt.Printf("> %s\n", name)
 		}
 
-		var defaultPrincipal string
-		fmt.Printf("Which of the above principals do you want to use as the default? ")
-		_, err = fmt.Scanln(&defaultPrincipal)
+		defaultPrincipal, err := cmdutil.ReadFromTerm("Which of the above principals do you want to use as the default?", -1, func(s string) bool {
+			s = strings.TrimSpace(s)
+			_, exists := cfg.Contexts.Principals[s]
+			return exists && s != "" && !strings.Contains(s, " ")
+		})
 		if err != nil {
-			cmdutil.Fatal("Error reading input: %v", err)
+			cmdutil.Fatal("%v", err)
 		}
+		defaultPrincipal = strings.TrimSpace(defaultPrincipal)
 
-		_, exists := cfg.Contexts.Principals[defaultPrincipal]
-		for !exists {
-			fmt.Printf("Invalid input, selected principal %s is not a valid principal: ", defaultPrincipal)
-			_, err = fmt.Scanln(&defaultPrincipal)
-			if err != nil {
-				cmdutil.Fatal("Error reading input: %v", err)
-			}
-			_, exists = cfg.Contexts.Principals[defaultPrincipal]
-		}
 		cfg.DefaultPrincipal = defaultPrincipal
 		fmt.Println()
 	}
