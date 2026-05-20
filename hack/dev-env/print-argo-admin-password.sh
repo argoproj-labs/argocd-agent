0332681047760c2bc83dec4be6e35c2412e8136e
Rizwana777 <rizwananaaz177@gmail.com>
2026-05-20 20:18:22 +0530

    replace ClusterRoleBinding patches with NamespaceTransformer
    Signed-off-by: Rizwana777 <rizwananaaz177@gmail.com>

--- hack/dev-env/print-argo-admin-password.sh
+++ hack/dev-env/print-argo-admin-password.sh
@@ -3,6 +3,6 @@
 set -eo pipefail
 
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
-source ${SCRIPTPATH}/namespaces.sh
+. "${SCRIPTPATH}/namespaces.sh"
 
 kubectl --context vcluster-control-plane -n ${ARGOCD_PRINCIPAL_NAMESPACE} get secret argocd-initial-admin-secret -o jsonpath='{.data.password}' | base64 -d && echo
--- hack/dev-env/setup-vcluster-env.sh
+++ hack/dev-env/setup-vcluster-env.sh
@@ -148,9 +148,9 @@ apply() {
     (cd $TMP_DIR/control-plane && kustomize edit set namespace ${ARGOCD_PRINCIPAL_NAMESPACE})
     (cd $TMP_DIR/agent-managed && kustomize edit set namespace ${ARGOCD_MANAGED_NAMESPACE})
     (cd $TMP_DIR/agent-autonomous && kustomize edit set namespace ${ARGOCD_AUTONOMOUS_NAMESPACE})
-    sed -i.bak -e "s/PrincipalNamespacePlaceholder/${ARGOCD_PRINCIPAL_NAMESPACE}/" $TMP_DIR/control-plane/clusterrolebinding-namespace-patch.yaml
-    sed -i.bak -e "s/ManagedNamespacePlaceholder/${ARGOCD_MANAGED_NAMESPACE}/" $TMP_DIR/agent-managed/clusterrolebinding-namespace-patch.yaml
-    sed -i.bak -e "s/AutonomousNamespacePlaceholder/${ARGOCD_AUTONOMOUS_NAMESPACE}/" $TMP_DIR/agent-autonomous/clusterrolebinding-namespace-patch.yaml
+    sed -i.bak -e "s/PrincipalNamespacePlaceholder/${ARGOCD_PRINCIPAL_NAMESPACE}/" $TMP_DIR/control-plane/namespace-transformer.yaml
+    sed -i.bak -e "s/ManagedNamespacePlaceholder/${ARGOCD_MANAGED_NAMESPACE}/" $TMP_DIR/agent-managed/namespace-transformer.yaml
+    sed -i.bak -e "s/AutonomousNamespacePlaceholder/${ARGOCD_AUTONOMOUS_NAMESPACE}/" $TMP_DIR/agent-autonomous/namespace-transformer.yaml
 
     # Generate the server secret key for the argocd running on the managed and autonomous agent clusters
     echo "-> Generate server.secretkey for agent's argocd-secrets"
